3d06abd9e206b853a77848420e502f7882a10648
Saveliy <sava2698@gmail.com>
2023-10-06 16:14:39 +0600

    added about-us section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/index.scss';
 import './styles/header.scss';
 import './styles/mainScreen.scss';
+import './styles/aboutUs.scss';
 import './styles/darkMode.scss';
 
 // const modeBtn = document.getElementById('mode_btn');
