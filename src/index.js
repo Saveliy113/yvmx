54c824ccf5da1d9e7c16f9f7172581cbbf95b85b
Saveliy <sava2698@gmail.com>
2023-10-09 13:03:33 +0600

    Added burger menu

--- src/index.js
+++ src/index.js
@@ -12,6 +12,27 @@ import './styles/contacts.scss';
 import './styles/darkMode.scss';
 
 // const modeBtn = document.getElementById('mode_btn');
+const mobileMenuBtn = document.getElementById('mobile_menu-btn');
+const navMenu = document.querySelector('.header_nav');
+console.log(mobileMenuBtn);
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileMenuBtn.classList.toggle('isOpened');
+
+  if (mobileMenuBtn.classList.value === 'isOpened') {
+    navMenu.classList.add('opened');
+  } else {
+    navMenu.classList.remove('opened');
+    navMenu.classList.add('closing');
+    navMenu.addEventListener(
+      'animationend',
+      () => {
+        navMenu.classList.remove('closing');
+      },
+      { once: true }
+    );
+  }
+});
 
 // modeBtn.addEventListener('click', (event) => {
 //   document.body.classList.toggle('dark');
